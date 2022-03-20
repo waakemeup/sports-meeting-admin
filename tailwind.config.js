e0583a7da5eda23bf784b0ec36da7beb6e80976a
Ruby <LIU10032@163.com>
2022-03-20 13:46:05 +0800

    fix: should add colors and media query at extends in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    screens: {
-      xxm: { max: "312px" }, //verysmall
-      mdm: { max: "880px" }, //md small
-      ldm: { max: "980px" },
-    },
-    colors: {
-      "system-gray": "#eff2f5",
+    extend: {
+      screens: {
+        xxm: { max: "312px" }, //verysmall
+        mdm: { max: "880px" }, //md small
+        ldm: { max: "980px" },
+      },
+      colors: {
+        "system-gray": "#eff2f5",
+      },
     },
   },
   plugins: [],
